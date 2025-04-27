1db92c5a379eb1459764ad3284e8b3fd778167f9
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-04-27 18:40:47 +0200

    configuration d'une session de base

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 import express from 'express';
+import session from 'express-session';
 const app = express();
 const PORT = 3000;
 
+app.use(
+  session({
+    secret: 'mdp2ouf',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.get('/', (req, res) => {
   res.send('Bienvenue sur le serveur !');
+});
+
+app.get('/login', (req, res) => {
+  req.session.user = { id: 1, name: 'Arthur' }; // Stocker des données dans la session
+  res.send('Vous êtes connecté !');
+});
+
+app.get('/profile', (req, res) => {
+  if (req.session.user) {
+    res.send(`Bonjour ${req.session.user.name}`);
+  } else {
+    res.send('Veuillez vous connecter.');
+  }
 });
 
 app.listen(PORT, () => {
