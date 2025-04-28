cc6c1905bd2d189b1b3c450ed7d70e359df5b745
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-04-28 13:14:09 +0200

    Ajout de la gestion des utilisateurs avec une base de données, intégration de dotenv pour la configuration des variables d'environnemen.

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ import session from 'express-session';
 import passport from 'passport';
 import { Strategy as LocalStrategy } from 'passport-local';
 import bcrypt from 'bcrypt';
+import client from './utils/db.js';
 
 const app = express();
 const PORT = 3000;
-
-// Fausse base de données d'utilisateurs
-const users = [
-  {
-    id: 1,
-    username: 'arthur',
-    password: bcrypt.hashSync('lalala', 10),
-  },
-];
 
 // Configuration de la session
 app.use(
@@ -28,15 +20,24 @@ app.use(
 
 // Configuration de Passport
 passport.use(
-  new LocalStrategy((username, password, done) => {
-    const user = users.find((u) => u.username === username);
-    if (!user) {
-      return done(null, false, { message: 'Utilisateur non trouvé.' });
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const result = await client.query(
+        'SELECT * FROM users WHERE username = $1',
+        [username]
+      );
+      const user = result.rows[0];
+
+      if (!user) {
+        return done(null, false, { message: 'Utilisateur non trouvé.' });
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, { message: 'Mot de passe incorrect.' });
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error);
     }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return done(null, false, { message: 'Mot de passe incorrect.' });
-    }
-    return done(null, user);
   })
 );
 
@@ -44,9 +45,16 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-  const user = users.find((u) => u.id === id);
-  done(null, user);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const result = await client.query('SELECT * FROM users WHERE id = $1', [
+      id,
+    ]);
+    const user = result.rows[0];
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
 });
 
 app.use(passport.initialize());
@@ -75,6 +83,22 @@ app.get('/profile', (req, res) => {
     res.send(`Bonjour ${req.user.username}`);
   } else {
     res.send('Veuillez vous connecter.');
+  }
+});
+
+// Route pour l'inscription
+app.post('/register', async (req, res) => {
+  const { username, password, email } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  try {
+    const result = await client.query(
+      'INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *',
+      [username, hashedPassword, email]
+    );
+    res.status(201).send('Utilisateur créé avec succès.');
+  } catch (error) {
+    res.status(500).send("Erreur lors de la création de l'utilisateur.");
   }
 });
 
