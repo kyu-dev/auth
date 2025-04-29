aa8332d60fc18ba56d988f394c9c9201e0c991f2
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-04-29 14:46:43 +0200

    Refactorisation des routes d'authentification : suppression de l'ancien fichier auth.js, création de authRoutes.js pour une meilleure organisation et ajout de la gestion des utilisateurs (connexion, déconnexion, profil, inscription) dans authController.js.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import './config/passport.js';
-import authRoutes from './routes/auth.js';
+import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = 3000;
@@ -24,11 +24,6 @@ app.use(passport.session());
 
 //routes
 app.use('/auth', authRoutes);
-
-// Route de test
-app.get('/', (req, res) => {
-  res.send('Serveur en marche !');
-});
 
 app.listen(PORT, () => {
   console.log('🚀 Serveur en écoute sur le port 3000 !');
