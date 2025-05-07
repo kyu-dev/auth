d6f4a8f4a492ce22d5ea4a02fcf1683cdbe27718
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-05-07 17:08:59 +0200

    refacto pour express

--- server.js
+++ server.js
@@ -3,11 +3,40 @@ import session from 'express-session';
 import passport from 'passport';
 import './config/passport.js';
 import authRoutes from './routes/authRoutes.js';
+import cors from 'cors';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const PORT = 3000;
 
+// Configuration de Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: "Documentation de l'API pour l'authentification",
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'], // Chemin vers les fichiers de routes
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
 // Middleware
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Le port de ton front avec Vite
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(
@@ -24,6 +53,9 @@ app.use(passport.session());
 
 //routes
 app.use('/auth', authRoutes);
+
+// Route pour la documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log('🚀 Serveur en écoute sur le port 3000 !');
